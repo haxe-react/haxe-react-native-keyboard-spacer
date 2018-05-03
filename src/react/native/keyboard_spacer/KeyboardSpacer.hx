abfe8dd5246ff010139e20324508b96f682765a6
Kevin Leung <kevinresol@gmail.com>
2018-05-03 21:49:33 +0800

    Type it

--- src/react/native/keyboard_spacer/KeyboardSpacer.hx
+++ src/react/native/keyboard_spacer/KeyboardSpacer.hx
@@ -1,4 +1,11 @@
 package react.native.keyboard_spacer;
 
+import react.ReactComponent;
+import haxe.Constraints;
+
 @:jsRequire('react-native-keyboard-spacer', 'default')
-extern class KeyboardSpacer extends react.ReactComponent {}
+extern class KeyboardSpacer extends ReactComponentOfProps<{
+	?topSpacing:Float,
+	?animationConfig:Dynamic,
+	?onToggle:Function,
+}> {}
